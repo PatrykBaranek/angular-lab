25c9aaf01661e39083059b2ef7402f04cf87c958
Patryk Baranek <cien2460045@gmail.com>
2023-03-20 21:36:25 +0100

    lab2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Card {
+  icon: string;
+  title: string;
+  value: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'lab1';
+  pageTitle: string = 'Charts';
+
+  cards: Card[] = [
+    {
+      icon: '',
+      title: 'Card 1',
+      value: 21,
+    },
+    {
+      icon: 'placeholder',
+      title: 'Card 2',
+      value: 22,
+    },
+    {
+      icon: 'placeholder',
+      title: 'Card 3',
+      value: 23,
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { RevenueComponent } from './revenue/revenue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HeaderComponent,
+    RevenueComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
